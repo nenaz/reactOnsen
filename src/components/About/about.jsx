627d1c4e1189492f8632fedf55cbededdc017b7f
nenaz <mr444@mail.ru>
2018-02-21 00:19:49 +0400

    FIX

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -21,11 +21,14 @@ class About extends Component{
                 }}
             >
                 <section
-                    style={{
-                        height: '85%'
-                    }}
                 >
                     <div>About</div>
+                </section>
+                <section style={{
+                    height: '74%',
+                    overflow: 'scroll',
+                    margin: '10px 0'
+                }}>
                     <div style={{
                         padding: '8px'
                     }}>Icons made by <a href="https://www.flaticon.com/authors/eleonor-wang" title="Eleonor Wang">Eleonor Wang</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
