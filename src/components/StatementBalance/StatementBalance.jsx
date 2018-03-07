832f01cc27d013e0d286199fcced4598033bfc63
Aleksey Gribov <gribov_an@open.ru>
2018-03-07 14:52:49 +0300

    В виджет баланса добавлены показатели общего уходи и прихода. Пока не точные

--- src/components/StatementBalance/StatementBalance.jsx
+++ src/components/StatementBalance/StatementBalance.jsx
@@ -1,19 +1,40 @@
 import React,{ Component} from 'react'
 import Balance from './Balance'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 
 class StatementBalance extends Component{
     constructor(props){
         super(props)
         this.state ={}
+        
+        this.getTodayBalance = this.getTodayBalance.bind(this)
+    }
+
+    getTodayBalance() {
+        let amountUp = 0
+        let amountDown = 0
+        const itemUp = this.props.operations.filter((item) => {
+            return item.typeOperation === "1"
+        })
+        const itemDown = this.props.operations.filter((item) => {
+            return item.typeOperation === "0"
+        })
+        itemUp.map(item => amountUp += item.balance * 1)
+        itemDown.map(item => amountDown += item.balance * 1)
+        return ({
+            amountUp,
+            amountDown
+        })
     }
 
     render(){
+        const balanceObj = this.getTodayBalance()
         return (
             <section className="nzBalanceData">
-                <Balance amount="999" />
+                <Balance amount={balanceObj.amountUp} />
                 <Balance amount={this.props.balance} typeBalance/>
-                <Balance amount="1000" separator />
+                <Balance amount={balanceObj.amountDown} separator />
             </section>
         )
     }
@@ -23,4 +44,6 @@ StatementBalance.propTypes = {
     balance: PropTypes.string
 }
 
-export default StatementBalance
+export default connect((state) => ({
+    operations: state.changeLastOperations
+}))(StatementBalance)
