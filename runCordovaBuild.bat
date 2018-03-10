96b85f0a586fcb90f4ec2f32b229df3718231b51
nenaz <mr444@mail.ru>
2018-03-10 21:08:02 +0300

    auto build for widows

---
+++ runCordovaBuild.bat
@@ -0,0 +1,18 @@
+
+REM npm run build
+
+sed -n "s/'a'/'AAA'/g" c:\Work\JS\ReactJS\convReact\reactOnsen\build\index.html
+
+REM rm -R /Users/Worker/Desktop/GRIBOV/cordova/react-app/www
+
+REM mkdir /Users/Worker/Desktop/GRIBOV/cordova/react-app/www
+
+REM cp -a ./build/ /Users/Worker/Desktop/GRIBOV/cordova/react-app/www
+
+REM XCOPY c:\Work\JS\ReactJS\convReact\reactOnsen\build c:\Work\Cordova\reactOnsen\project1\www /H /Y /C /R /S
+
+REM cd c:\Work\Cordova\reactOnsen\project1
+
+REM cordova build android
+
+REM # cordova run android
