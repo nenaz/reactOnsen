69c2a4188009aa869c5beae6184e510eb1b1f7c5
Aleksey Gribov <gribov_an@open.ru>
2018-05-08 11:28:49 +0300

    настройка тестов

---
+++ src/__test__/test_Link.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { shallow } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
+
+import Link from '../test_component/Link';
+
+describe('Link', () => {
+    it('should render correctly', () => {
+        const output = shallow(
+            <Link title="mockTitle" url="mockUrl" />
+        );
+        expect(shallowToJson(output)).toMatchSnapshot();
+    });
+});
